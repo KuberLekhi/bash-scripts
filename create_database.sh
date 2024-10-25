220cc888c2c6055f939ee31a26b9ab903e5e1b31
Kuber Lekhi <155307227+KuberLekhi@users.noreply.github.com>
2024-10-26 01:41:10 +0530

    Update create_database.sh

--- create_database.sh
+++ create_database.sh
@@ -7,6 +7,6 @@ then
     sudo apt-get install sqlite3
 fi
 
-# Create a SQLite database and a table to store data
-sqlite3 mydatabase.db "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, temperature REAL, humidity REAL);"
+# Create a SQLite database and a table with the desired structure
+sqlite3 mydatabase.db "CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, favourite_colour TEXT);"
 echo "Database and table created!"
